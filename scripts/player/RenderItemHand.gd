b457abb64abc25e8483495b06da49ef1baf467d2
David Gabriel Lacerda Dutra <davidlacerdadutra123@gmail.com>
2026-03-19 19:00:09 -0300

    .

---
+++ scripts/player/RenderItemHand.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+@export var item_data: Item
+
+@onready var render_modelo_item: Node3D = $"."
+
+
+func _ready() -> void:
+	render_item_hand()
+func _process(delta: float) -> void:
+		pass
+
+	
+func render_item_hand():
+	if item_data and item_data.caminho_modelo:
+		var instancia = item_data.caminho_modelo.instantiate()
+		render_modelo_item.add_child(instancia)
